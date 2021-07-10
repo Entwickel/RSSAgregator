a1b2dacab037a9990b0201b900788b85527bfa2f
Pierre Lamendin <44493820+Entwickel@users.noreply.github.com>
2021-07-10 13:33:15 +0200

    Add Class Agregator

---
+++ RSSAgregator/RSSAgregator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSSAgregator
+{
+    class RSSagregator
+    {
+        public int x_fenetre;
+        public int y_fenetre;
+
+
+        public RSSagregator(int x, int y)
+        {
+            x_fenetre = x;
+            y_fenetre = y;
+
+        }
+
+    }
+}
